5cb70e66f1bdaa4c130dd09a4c58a7f04b71add6
Santiago Nicolas De Giacomi <santidegiacomi@gmail.com>
2023-01-17 21:58:36 -0300

    ultima version

--- src/Componentes/ItemListContainer/ItemListContainer.js
+++ src/Componentes/ItemListContainer/ItemListContainer.js
@@ -1,28 +1,42 @@
 import React,{useState,useEffect} from 'react'
 import Data from '../Data/Data.json'
-import ItemList from '../ItemList/ItemList'
+import ItemDetail from '../ItemDetail/ItemDetail'
+import {useParams} from 'react-router'
+
 const ItemListContainer = () => {
-        const [productos, setproductos] = useState([])
-        const promise = () => {
-            return new Promise((resolve) => {
-                setTimeout(() => {
-                    resolve(Data);
-                },2000);
-                })
-            }
-        useEffect(() => {
-            promise().then((Data) => {
-                setproductos(Data);
+    const [producto, setproducto] = useState({})
+    const {Id} = useParams();
+    const getProducts = () => {
+        return new Promise ((res) => {
+            setTimeout(() => {
+                res(Data)
+            }, 2000);
+        });
+    }
+    const getProductsByCategory = (categoria) => {
+        return new Promise((res) => {
+            const productos = Data.filter(producto => producto.categoria === categoria);
+            setTimeout(() => {
+                res(productos);
+            }, 2000);
+        });
+    }
+    useEffect(() => {
+        if(Id){
+            getProductsByCategory(Id).then((producto) => {
+                setproducto(producto)
             })
-        }, [
-            
-        ])
-        
+        }else{
+            getProducts.then((producto) => {
+                setproducto(producto);
+            })
+        }
+    },)
     
-    
+
     return (
         <div>
-        <ItemList productos={productos}/>
+            <ItemDetail producto={producto}/>
         </div>
     )
 }
